b6d9551f8ec2a8ed0ba035e1133f3717045daffb
Saurabh Prakash <saurabh.prakash@tallysolutions.com>
2022-06-23 23:18:22 +0530

    Add program finding nth fibonacci using recursion

---
+++ recursive_fib.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// Numbering starts from 1
+int fib(int n)
+{
+    if (n <= 2)
+        return 1;
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n;
+
+    cout << "Press 0 to exit\n";
+
+    cout << "\nEnter n : ";
+    cin >> n;
+
+    while (n && n > 0)
+    {
+
+        cout << "fib (" << n << ") : ";
+        cout << fib(n) << endl;
+
+        cout << "\n===================================\n";
+
+        cout << "\nEnter n : ";
+        cin >> n;
+    }
+
+    return 0;
+}
