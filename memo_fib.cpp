7003e66c2d2801775db08a1ddadccbdd2062cb79
Saurabh Prakash <saurabh.prakash@tallysolutions.com>
2022-06-24 23:50:49 +0530

    Solve nth fibonacci number using memoization

---
+++ memo_fib.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+unsigned long long memo[1000];
+
+// Numbering starts from 1
+unsigned long long fib(int n)
+{
+    if (memo[n - 1])
+    {
+
+        return memo[n - 1];
+    }
+
+    memo[n - 1] = fib(n - 1) + fib(n - 2);
+    return memo[n - 1];
+}
+
+int main()
+{
+    int n;
+
+    memo[0] = 1;
+    memo[1] = 1;
+
+    cout << "Press 0 to exit\n";
+
+    cout << "\nEnter n : ";
+    cin >> n;
+
+    while (n && n > 0)
+    {
+
+        cout << "fib (" << n << ") : ";
+        cout << fib(n) << endl;
+
+        cout << "\n===================================\n";
+
+        cout << "\nEnter n : ";
+        cin >> n;
+    }
+
+    return 0;
+}
